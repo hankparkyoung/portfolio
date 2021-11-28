79ca4b7f68b16d539080ed6b6123c2270fc1e9fc
Hank Park <hankparkyoung@gmail.com>
2021-11-27 20:03:09 -0500

    feat: :sparkles: my story
    my story section for portfolio site

--- src/pages/main.jsx
+++ src/pages/main.jsx
@@ -32,8 +32,28 @@ return (
       </div>
     </div>
   </div>
-  <div className="intro">
-    <p>Work in progress</p>
+  <div className='about'>
+    <div className='about__overlay'>
+      <div className='about__card'>
+        <h2 className='about__title'>
+          my story
+        </h2>
+        <p className='about__text'>
+          Not just a software developer, but also a board game enthusiast, lover of puns, and a
+          friend you can always lean on.
+        </p>
+        <p className='about__text'>
+          My experience is both lived and learned, but what ties it all together is the excitement
+          towards solving human-focused problems with the tools I have at my disposal. 
+        </p>
+        <p className='about__text'>
+          Whether that's building a web application with my most comfortable tech stack, or it's
+          listening to a colleague or friend and offering support and advice, I'm passionate about
+          doing my best!
+        </p>
+        
+      </div>
+    </div>
   </div>
 </div>
 );
